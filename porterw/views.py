352db3b981e9cbb4deb07dd614cca5c51ba6a69f
Victor Ziv <victor@colabo.com>
2016-12-21 18:17:56 +0000

    Change index massage a bit

--- porterw/views.py
+++ porterw/views.py
@@ -4,4 +4,4 @@ from porterw import app
 @app.route('/index')
 @app.route('/home')
 def index():
-    return "Hello Bla! Don't you want to give in?"
+    return "Hello once again! You gave in eventuall, ha?"
