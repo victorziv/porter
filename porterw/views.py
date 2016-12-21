d24611b981eb812adbdd21c0260cba54965b125c
Victor Ziv <victor@colabo.com>
2016-12-21 18:27:57 +0000

    Connecting to mongodb

--- porterw/views.py
+++ porterw/views.py
@@ -1,7 +1,21 @@
 from porterw import app
+import pymongo
+
+HOST='porter-mongodb'
+PORT=48084
+DB='porter'
+
+# _______________________________________
+
+def connectdb():
+    client = pymongo.MongoClient(HOST, PORT)
+    return getattr(client, DB)
+
+# _______________________________________
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return "Hello once again! You gave in eventuall, ha?"
+    db = connectdb()
+    return "Connected to %r" % db
