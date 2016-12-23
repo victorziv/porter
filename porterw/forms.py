d4c95dfde7c42a8ddbcfd9376ff7fd84d1b630ca
Victor Ziv <victor@colabo.com>
2016-12-23 17:55:02 +0000

    Add placeholder for flashed messages

--- porterw/forms.py
+++ porterw/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 # =========================================
 
-class LoginForm(Form):
-    opendid = StringField('openid', validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
--- porterw/views.py
+++ porterw/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from porterw import app
 from porterw.forms import LoginForm
 import pymongo
@@ -31,4 +31,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for OpenId=%s, remember_me=%s" % ( form.openid.data, str(form.remember_me.data)))
+        return redirect("/index")
     return render_template('login.html', title='Sign In', form=form)
